bba02d00043c43923aa2ed1189bac9bbf1113359
SaiKrishna-official <68114063+SaiKrishna-official@users.noreply.github.com>
2020-09-20 14:53:21 +0530

    restaurant info

--- FancyFood/app/src/main/java/com/example/fancyfood/MainActivity.java
+++ FancyFood/app/src/main/java/com/example/fancyfood/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.fancyfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText edYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activty_main);
+    }
+    public void nextButton(View view){
+
     }
 }
