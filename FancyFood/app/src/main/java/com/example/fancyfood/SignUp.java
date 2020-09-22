d526a258cd0c2677529f50f637bc4a5822aa307a
Cyb3rM3rlin <starlordkrishna05@gmail.com>
2020-09-22 13:29:53 +0530

    SignUp page design complete

--- FancyFood/app/src/main/java/com/example/fancyfood/SignUp.java
+++ FancyFood/app/src/main/java/com/example/fancyfood/SignUp.java
@@ -3,12 +3,29 @@ package com.example.fancyfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SignUp extends AppCompatActivity {
-
+    EditText etEmail,etPassword,etConfirm;
+   TextView tvSignIn;
+   FloatingActionButton buSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirm = findViewById(R.id.etConfirm);
+        tvSignIn = findViewById(R.id.tvSignIn);
+        buSignUp = findViewById(R.id.buSignUp);
+    }
+
+
+    public void SignUpAction(View view) {
+
     }
 }
