97ecfb709c1aba777ead2351a7137b78cf9f9e45
SaiKrishna-official <68114063+SaiKrishna-official@users.noreply.github.com>
2020-09-23 13:37:38 +0530

    updated files 1

--- FancyFood/app/src/main/java/com/example/fancyfood/ReviewDetails.java
+++ FancyFood/app/src/main/java/com/example/fancyfood/ReviewDetails.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,14 +26,18 @@ import java.util.ArrayList;
 public class ReviewDetails extends AppCompatActivity {
 
     ArrayList<Uri> imagesUriList;
+    Bundle bundle;
     LinearLayout linearLayout,ll;
     TextView resName,resAddress,resYear,resArea,resPhoneno,resCuisines;
+    FirebaseDatabase fd;
+    DatabaseReference database;
+
     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_details);
-        Bundle bundle = getIntent().getExtras();
+        bundle = getIntent().getExtras();
         imagesUriList = (ArrayList<Uri>) getIntent().getSerializableExtra("uriList");
         linearLayout = findViewById(R.id.ll_images);
         ll = findViewById(R.id.review_res_info);
@@ -42,12 +47,12 @@ public class ReviewDetails extends AppCompatActivity {
         resArea = findViewById(R.id.review_res_area);
         resPhoneno = findViewById(R.id.review_res_phoneno);
         resCuisines = findViewById(R.id.review_res_cuisines);
-            resName.setText(bundle.getString("resName"));
-            resAddress.setText(bundle.getString("resAddress"));
-            resYear.setText(bundle.getString("resYear"));
-            resArea.setText(bundle.getString("resArea"));
-            resPhoneno.setText(bundle.getString("resPhoneno"));
-            resCuisines.setText(bundle.getString("resCuisines"));
+        resName.setText(bundle.getString("resName"));
+        resAddress.setText(bundle.getString("resAddress"));
+        resYear.setText(bundle.getString("resYear"));
+        resArea.setText(bundle.getString("resArea"));
+        resPhoneno.setText(bundle.getString("resPhoneno"));
+        resCuisines.setText(bundle.getString("resCuisines"));
         if(imagesUriList.size()!=0)
         {
             for(int i=0;i<imagesUriList.size();i++)
@@ -60,5 +65,18 @@ public class ReviewDetails extends AppCompatActivity {
         iv.setLayoutParams(params);
         iv.setImageURI(uri);
         linearLayout.addView(iv);
+    }
+    public void createUser(View view){
+        String rName = bundle.getString("resName");
+        String rAddress = bundle.getString("resAddress");
+        String rArea = bundle.getString("reArea");
+        String rYear = bundle.getString("resYear");
+        String rPhoneno = bundle.getString("resPhoneno");
+        String rCuisines = bundle.getString("resCuisines");
+        CreateUser cUser = new CreateUser(rName,rAddress,rYear,rPhoneno,rArea,rCuisines);
+        fd = FirebaseDatabase.getInstance();
+        database = fd.getReference("Restaurant");
+        database.child(rPhoneno).setValue(cUser);
+
     }
 }
