83fd139a21f42f4ef5656a3346be797ce6d5a38c
SaiKrishna-official <68114063+SaiKrishna-official@users.noreply.github.com>
2020-09-25 20:49:12 +0530

    updated files 1

--- FancyFood/app/src/main/java/com/example/fancyfood/RestaurantInfo.java
+++ FancyFood/app/src/main/java/com/example/fancyfood/RestaurantInfo.java
@@ -1,48 +1,57 @@
 package com.example.fancyfood;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
-import android.widget.ViewFlipper;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class RestaurantInfo extends AppCompatActivity {
-    EditText rName,rAddress,rPhoneNo,rYear,rCuisines,rArea;
+    String name, address, year, area, phoneno, cuisines;
+    EditText rName, rAddress, rPhoneNo, rYear, rCuisines, rArea;
     public ArrayList<Uri> imageUri = new ArrayList<>();
     final int PICK_IMAGE = 1;
+    String  uid = UUID.randomUUID().toString();
     LinearLayout linearLayout;
+    int i = 0, j = 50;
+    Uri[] uriArray = new Uri[50];
     Bundle bundle = new Bundle();
-    int i = 1 , j = 51;
+    FirebaseDatabase fd;
+    FirebaseStorage fStorage;
+    DatabaseReference database;
+    StorageReference storage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_info);
+        for (Uri u : uriArray) {
+            u = null;
+        }
         linearLayout = findViewById(R.id.ll);
         rName = findViewById(R.id.restaurant_name);
         rAddress = findViewById(R.id.restaurant_address);
@@ -53,10 +62,11 @@ public class RestaurantInfo extends AppCompatActivity {
 
 
     }
-    public void browsingFiles(View view){
-                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
-                gallery.setType("image/*");
-                startActivityForResult(Intent.createChooser(gallery,"Pick an image"),PICK_IMAGE);
+
+    public void browsingFiles(View view) {
+        Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+        gallery.setType("image/*");
+        startActivityForResult(Intent.createChooser(gallery, "Pick an image"), PICK_IMAGE);
 
     }
 
@@ -67,50 +77,109 @@ public class RestaurantInfo extends AppCompatActivity {
             viewImages(data.getData());
         }
     }
-    public void viewImages(Uri uri){
-        imageUri.add(uri);
+
+    public void viewImages(final Uri uri) {
+        uriArray[i] = uri;
+        final Button b = new Button(this);
         ImageView iv = new ImageView(this);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        iv.setLayoutParams(params);
         iv.setImageURI(uri);
+        b.setBackgroundResource(R.drawable.ic_delete1);
+        iv.setId(i);
+        b.setId(j);
+        i++;
+        j++;
         linearLayout.addView(iv);
+        linearLayout.addView(b);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("id", String.valueOf(b.getId()));
+                linearLayout.removeView(findViewById((b.getId()) - 50));
+                linearLayout.removeView(findViewById(b.getId()));
+                uriArray[(b.getId()) - 50] = null;
+                uploadImages(phoneno);
+            }
+        });
     }
-   public void nextButton(View view){
-                if(rName.length()==0)
-                    rName.setError("Name cannot be empty");
-                if(rAddress.length()==0)
-                    rAddress.setError("Address cannot be empty");
-                if(rPhoneNo.length()==0)
-                    rPhoneNo.setError("Phone number cannot be empty");
-                if(rYear.length()==0)
-                    rYear.setError("Year cannot be empty");
-                if(rCuisines.length()==0)
-                    rCuisines.setError("Cuisines cannot be empty");
-                if(rArea.length()==0)
-                    rArea.setError("Area cannot be empty");
-                if(imageUri.size()==0)
-                    Toast.makeText(this,"Please provide some photos",Toast.LENGTH_LONG).show();
-                if(rName.length()!=0&&rAddress.length()!=0&&rPhoneNo.length()!=0&&rYear.length()!=0&&rCuisines.length()!=0&&rArea.length()!=0&&imageUri.size()!=0){
-                    resInfo();
-                    Intent i = new Intent(RestaurantInfo.this,ReviewDetails.class);
-                    i.putExtras(bundle);
-                    i.putExtra("uriList",imageUri);
-                    startActivity(i);
-                }
+
+    public void nextButton(View view) {
+        for (Uri uri : uriArray) {
+            if (uri != null) {
+                imageUri.add(uri);
+                uri = null;
+            }
+        }
+        Set<Uri> s = new LinkedHashSet<>();
+        s.addAll(imageUri);
+        imageUri.clear();
+        imageUri.addAll(s);
+        if (rName.length() == 0)
+            rName.setError("Name cannot be empty");
+        if (rAddress.length() == 0)
+            rAddress.setError("Address cannot be empty");
+        if (rPhoneNo.length() == 0)
+            rPhoneNo.setError("Phone number cannot be empty");
+        if (rYear.length() == 0)
+            rYear.setError("Year cannot be empty");
+        if (rCuisines.length() == 0)
+            rCuisines.setError("Cuisines cannot be empty");
+        if (rArea.length() == 0)
+            rArea.setError("Area cannot be empty");
+        if (imageUri.size() == 0)
+            Toast.makeText(this, "Please provide some photos", Toast.LENGTH_LONG).show();
+        if (rName.length() != 0 && rAddress.length() != 0 && rPhoneNo.length() != 0 && rYear.length() != 0 && rCuisines.length() != 0 && rArea.length() != 0 && imageUri.size() != 0) {
+            resInfo();
+            storage = fStorage.getReference("Restaurant Images");
+            StorageReference ref = storage.child(uid).child(imageUri.get(0).toString());
+            ref.delete();
+            Intent i = new Intent(RestaurantInfo.this, ReviewDetails.class);
+            i.putExtras(bundle);
+            i.putExtra("uriList", imageUri);
+            startActivity(i);
+        }
     }
-    public void resInfo(){
-        String name,address,year,area,phoneno,cuisines;
+
+    public void resInfo() {
         name = rName.getText().toString();
         address = rAddress.getText().toString().trim();
         year = rYear.getText().toString().trim();
         area = rArea.getText().toString().trim();
         phoneno = rPhoneNo.getText().toString().trim();
-        cuisines =rCuisines.getText().toString().trim();
-        bundle.putString("resName",name);
-        bundle.putString("resAddress",address);
-        bundle.putString("resYear",year);
-        bundle.putString("resArea",area);
-        bundle.putString("resPhoneno",phoneno);
-        bundle.putString("resCuisines",cuisines);
+        cuisines = rCuisines.getText().toString().trim();
+        uploadDetails();
+    }
+    public void uploadDetails(){
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setTitle("Uploading Details....");
+        pDialog.show();
+        CreateUser cUser = new CreateUser(name,address,year,phoneno,area,cuisines);
+        fd = FirebaseDatabase.getInstance();
+        fStorage = FirebaseStorage.getInstance();
+        database = fd.getReference("Restaurant");
+        database.child(uid).setValue(cUser).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                pDialog.dismiss();
+                Toast.makeText(RestaurantInfo.this,"Uploading details is successful",Toast.LENGTH_LONG).show();
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pDialog.dismiss();
+                        Toast.makeText(RestaurantInfo.this,"Uploading details is unsuccessful",Toast.LENGTH_LONG).show();
+                    }
+                });
+        uploadImages(uid);
+    }
+    public void uploadImages(String uid){
+        storage = fStorage.getReference("Restaurant Images");
+        StorageReference ref =storage.child(uid);
+        for(int i=0;i<imageUri.size();i++) {
+            ref.child("image " + (i + 1)).putFile(imageUri.get(i));
+        }
+        Toast.makeText(RestaurantInfo.this,"Uploading images is successful",Toast.LENGTH_LONG).show();
     }
 }
--- FancyFood/app/src/main/java/com/example/fancyfood/ReviewDetails.java
+++ FancyFood/app/src/main/java/com/example/fancyfood/ReviewDetails.java
@@ -1,20 +1,18 @@
 package com.example.fancyfood;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,16 +24,12 @@ import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class ReviewDetails extends AppCompatActivity {
-
+public class ReviewDetails extends AppCompatActivity{
     public ArrayList<Uri> imagesUriList;
     Bundle bundle;
     LinearLayout linearLayout,ll;
     TextView resName,resAddress,resYear,resArea,resPhoneno,resCuisines;
-    FirebaseDatabase fd;
-    FirebaseStorage fStorage;
-    DatabaseReference database;
-    StorageReference storage;
+
     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,48 +68,10 @@ public class ReviewDetails extends AppCompatActivity {
         uploadResDetails();
     }
     public void uploadResDetails(){
-        final ProgressDialog pDialog = new ProgressDialog(this);
-        pDialog.setTitle("Uploading Details....");
-        pDialog.show();
-        String rName = bundle.getString("resName");
-        String rAddress = bundle.getString("resAddress");
-        String rArea = bundle.getString("reArea");
-        String rYear = bundle.getString("resYear");
-        String rPhoneno = bundle.getString("resPhoneno");
-        String rCuisines = bundle.getString("resCuisines");
-        final String uid = UUID.randomUUID().toString();
-        CreateUser cUser = new CreateUser(rName,rAddress,rYear,rPhoneno,rArea,rCuisines,uid);
-        fd = FirebaseDatabase.getInstance();
-        fStorage = FirebaseStorage.getInstance();
-        database = fd.getReference("Restaurant");
-        database.child(uid).setValue(cUser).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                pDialog.dismiss();
-                Toast.makeText(ReviewDetails.this,"Uploading details is successful",Toast.LENGTH_LONG).show();
 
-            }
-        })
-        .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                pDialog.dismiss();
-                Toast.makeText(ReviewDetails.this,"Uploading details is unsuccessful",Toast.LENGTH_LONG).show();
-            }
-        });
-        uploadResImages(uid);
     }
     public void uploadResImages(String uid){
-        final ProgressDialog piDialog = new ProgressDialog(this);
-        piDialog.setTitle("Uploading Restaurant Images");
-        piDialog.show();
-        storage = fStorage.getReference("Restaurant Images");
-        StorageReference ref =storage.child(uid);
-        for(int i=0;i<imagesUriList.size();i++) {
-            ref.child("image " + (i + 1)).putFile(imagesUriList.get(i));
-        }
-            piDialog.dismiss();
-            Toast.makeText(ReviewDetails.this,"Uploading images is successful",Toast.LENGTH_LONG).show();
+
     }
     public void review(MenuItem menuItem)
     {
@@ -124,8 +80,7 @@ public class ReviewDetails extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater m = new MenuInflater(this);
-        m.inflate(R.menu.revirew,menu);
+        getMenuInflater().inflate(R.menu.revirew,menu);
         return true;
     }
 }
